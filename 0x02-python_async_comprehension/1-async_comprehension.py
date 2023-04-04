5b422756faab351fdd77438c8c23b74217204217
joethesaint <joebamisaye068@gmail.com>
2023-04-04 18:48:39 -0400

    add task 1

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+Import async_generator from the previous task and write a
+coroutine called async_comprehension that takea no arguments.
+"""
+
+import asyncio
+import random
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> int[float]:
+    """
+    Coroutine collects 10 random numbers using an async
+    comprehensing over async_generator, then return the 10
+    random numbers
+    """
+    res = [i async for i in async_generator()]
+    return res
