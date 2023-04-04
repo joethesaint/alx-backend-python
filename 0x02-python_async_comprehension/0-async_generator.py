79d90aaaced5a7fb26f8f7d855d8ddfc0580aac4
joethesaint <joebamisaye068@gmail.com>
2023-04-04 18:35:34 -0400

    correct check 3 in task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,9 +5,9 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """ Produce a random number for every iteration"""
     for i in range(10):
         yield random.uniform(0, 10)
         await asyncio.sleep(1)
-        
