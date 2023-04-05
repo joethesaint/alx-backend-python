dcf802e31f8f2dee7c97d2a28d27879696ae0838
joethesaint <joebamisaye068@gmail.com>
2023-04-05 18:45:46 -0400

    add task 2 and 2-main.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-1. Let's execute multiple coroutinnes at the same time with async
+1. Let's execute multiple coroutines at the same time with async
 """
 import asyncio
 from typing import List
